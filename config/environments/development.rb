45a0751fbac8216ec1c1431b400c1d8d6c77975d
Jeff Harrington <jeff.harrington@gmail.com>
2014-09-03 22:51:19 -0500

    Fixing STDOUT

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,5 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  STDOUT.sync = true
 end
-STDOUT.sync = true
